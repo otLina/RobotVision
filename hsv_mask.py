b159baecda886750eeafa2f0c0933d18aa297d97
zeki31 <ozeki.f.h.13@gmail.com>
2022-12-08 15:38:15 +0900

    色指定->2値画像

--- hsv_mask.py
+++ hsv_mask.py
@@ -9,20 +9,28 @@ while True:
 
     # Webカメラのフレーム取得
     ret, frame = cap.read()
-    cv2.imshow("camera", frame)
 
     # 画像をRGBからHSVに変換
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-   # 色の範囲
-    HSVRange = {
-        "lower": np.array([50, 50, 30]), 
-        "upper": np.array([70, 120, 70])
-    }
+    def click_pos(event, x, y, flags, params):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            H,S,V=hsv[y,x,:]
+            hsv_str='(H,S,V)=('+str(H)+','+str(S)+','+str(V)+')'
+            print(hsv_str)
+            # 色の範囲
+            HSVRange = {
+                "lower": np.array([int(H)-10, int(S)-20, int(V)-20]), 
+                "upper": np.array([int(H)+10, int(S)+20, int(V)+20])
+            }
+            # HSVからマスクを作成
+            hsv_mask = cv2.inRange(hsv, HSVRange["lower"], HSVRange["upper"])
+            # medianblurを用いてノイズ成分を除去
+            blur_mask = cv2.medianBlur(hsv_mask, ksize=3)
+            cv2.imshow("hsv_mask", blur_mask)
 
-    # HSVからマスクを作成
-    hsv_mask = cv2.inRange(hsv, HSVRange["lower"], HSVRange["upper"])
-    cv2.imshow("hsv_mask", hsv_mask)
+    cv2.imshow('window', frame)
+    cv2.setMouseCallback('window', click_pos)
 
     # 終了オプション
     k = cv2.waitKey(1)
