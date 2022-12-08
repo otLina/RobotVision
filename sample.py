d8e3931f234ecbc5eb9bd9ac36a9a5e2c2c21da8
zeki31 <ozeki.f.h.13@gmail.com>
2022-12-08 11:37:20 +0900

    test

---
+++ sample.py
@@ -0,0 +1,22 @@
+# ライブラリのインポート
+import cv2
+import numpy as np
+
+# -----------以下記述-----------
+cap = cv2.VideoCapture(0)
+grayscale = False
+
+while(True):
+    ret,frame = cap.read()
+    if grayscale:
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        
+    cv2.imshow('camera',frame)
+    k = cv2.waitKey(1)
+    if k == ord('q'):
+        break
+    elif k == ord('g'):
+        grayscale = True
+    
+cap.release()
+cv2.destroyAllwindows()
